efb40e766a0674d6aa78f0d0ab5084e5873efb00
Brandon Seda <brandon@Brandons-MacBook-Air.local>
2023-07-29 22:13:13 -0400

    added styling and images to home page

--- models/user.js
+++ models/user.js
@@ -5,9 +5,11 @@ const userSchema = new mongoose.Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  password: { type: String, required: true, minLength: 8, select: false },
   age: { type: Number, min: 0, max: 120 },
   gamesPlayed: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Game' }],
+  photo: { type: String, default: '../images/user.png' },
+  role: { type: String, enum: ['user', 'leader', 'admin'], default: 'user' },
 });
 
 const User = mongoose.model('User', userSchema);
